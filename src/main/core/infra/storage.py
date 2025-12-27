cd65c74d04b7b99a9e68a55b04e5c15e43ee8bcb
caio-maximiano <caiomax97@hotmail.com>
2025-12-27 16:48:40 -0300

    Adjsuitng key

--- src/main/core/infra/storage.py
+++ src/main/core/infra/storage.py
@@ -1,26 +1,30 @@
-# src/main/core/infra/storage.py
-
+import os
 from azure.storage.filedatalake import DataLakeServiceClient
-from azure.identity import DefaultAzureCredential
 
 
 class Storage:
     def __init__(
         self,
-        account_name: str = "cnesstorageaccount",
+        account_name: str | None = None,
         file_system: str = "bronze",
     ):
-        self.account_name = account_name
+        self.account_name = account_name or os.getenv("STORAGE_ACCOUNT_NAME")
         self.file_system = file_system
 
-        credential = DefaultAzureCredential()
+        if not self.account_name:
+            raise ValueError("STORAGE_ACCOUNT_NAME não definido")
+
+        account_key = os.getenv("STORAGE_ACCOUNT_KEY")
+        if not account_key:
+            raise ValueError("STORAGE_ACCOUNT_KEY não definido")
 
         self.client = DataLakeServiceClient(
             account_url=f"https://{self.account_name}.dfs.core.windows.net",
-            credential=credential,
+            credential=account_key,
         )
 
         self.fs = self.client.get_file_system_client(self.file_system)
+
 
 # instâncias compartilhadas
 bronze = Storage(file_system="bronze")
